9ef2ea05982872eccaa5a4e338cb631fd8e05023
Simon Fink <simon.2.fink@gmail.com>
2020-04-17 16:11:30 +0200

    Make functional tests a package using LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
         # Simon has heard of a new web app, where he can enter things into a Todo
         # list, he goes to check out this page
 
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -72,6 +72,4 @@ class NewVisitorTest(unittest.TestCase):
         
         self.fail('Finish tests from above!!')
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
 
