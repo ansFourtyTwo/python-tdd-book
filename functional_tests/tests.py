03cd314fe64eb5817900d05028958b48214f0690
Simon Fink <simon.2.fink@gmail.com>
2020-04-17 16:41:57 +0200

    Remove magic sleep and use implicit wait with timeout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
         
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Simon has heard of a new web app, where he can enter things into a Todo
@@ -43,7 +54,7 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: Buy Hafermilch" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy Hafermilch')
+        self.wait_for_row_in_list_table('1: Buy Hafermilch')
         
         # self.assertTrue(
         #     any(row.text == '1: Buy Hafermilch' for row in rows),
@@ -56,11 +67,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Call mum')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on his list
-        self.check_for_row_in_list_table('1: Buy Hafermilch')
-        self.check_for_row_in_list_table('2: Call mum')
+        self.wait_for_row_in_list_table('1: Buy Hafermilch')
+        self.wait_for_row_in_list_table('2: Call mum')
 
         # Simon wonders wheter the site will remember his list. Then he sees
         # that the has generated a unique URL for him -- there is some
