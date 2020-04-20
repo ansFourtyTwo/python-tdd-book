4782476d2aa73339d743ceb0be281c1eca3ae3cf
Simon Fink <simon.2.fink@gmail.com>
2020-04-20 19:39:10 +0200

    Add URL and view to add new items to existing list (all test passing)

--- superlist/urls.py
+++ superlist/urls.py
@@ -25,8 +25,12 @@ urlpatterns = [
         views.new_list,
         name='new_list'
     ),
-    url(r'^lists/(.+)/$', 
+    url(r'^lists/(\d+)/$', 
         views.view_list, 
         name='view_list'
+    ),
+    url(r'^lists/(\d+)/add_item$',
+        views.add_item,
+        name='add_item'
     ),
 ]
