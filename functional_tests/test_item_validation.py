6781b4eed0a76b518d0873a2089d1904669ee24c
Simon Fink <simon.2.fink@gmail.com>
2020-05-03 13:55:24 +0200

    Implement functional tests for item validation

--- functional_tests/test_item_validation.py
+++ functional_tests/test_item_validation.py
@@ -7,15 +7,35 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Simon goes to the home page and accidentially tries to submit
         # an empty list item, He hits Enter on the empty input box
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
         
         # The home page refreshes, and there is an error message saying
         # the list items cannot be blank
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
         
         # She tries again with some text for the item, which now works
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
         
         # Perversly, she now decides to submit a second blank list item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
         
         # He receives a similar warning on the list page
-        
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
         # And he can correct it by filling some text in
-        self.fail('Write me')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Make tea')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
