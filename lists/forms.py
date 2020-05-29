8f89eae1be6829e8933f72bef1ea211bf43904f9
Simon Fink <simon.2.fink@gmail.com>
2020-05-29 18:37:55 +0200

    Using form for existing list in the views

--- lists/forms.py
+++ lists/forms.py
@@ -11,7 +11,7 @@ class ItemForm(forms.models.ModelForm):
     
     def save(self, for_list):
         self.instance._list = for_list
-        return super().save()
+        return super().save(self)
     
     class Meta:
         model = Item
@@ -31,6 +31,9 @@ class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.instance._list = for_list
+        
+    def save(self):
+        return forms.models.ModelForm.save(self)
         
     def validate_unique(self):
         try:
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,7 +7,10 @@ from unittest import skip
 
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm, ExistingListItemForm, EMPTY_ITEM_ERROR,
+    DUPLICATE_ITEM_ERROR
+)
 
 
 class HomePageTest(TestCase):
@@ -66,6 +69,11 @@ class NewListTest(TestCase):
 
 
 class ListViewTest(TestCase):
+    
+    # Helper methods
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(f'/lists/{list_.id}/',data={'text': ''})
         
     def test_uses_list_template(self):
         list_ = List.objects.create()
@@ -123,12 +131,11 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(
+            response.context['form'], 
+            ExistingListItemForm
+        )
         self.assertContains(response, 'name="text"')
-    
-    def post_invalid_input(self):
-        list_ = List.objects.create()
-        return self.client.post(f'/lists/{list_.id}/',data={'text': ''})
         
     def test_for_input_nothing_saved_to_db(self):
         response = self.post_invalid_input()
@@ -141,13 +148,15 @@ class ListViewTest(TestCase):
         
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(
+            response.context['form'], 
+            ExistingListItemForm
+        )
         
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
         
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(_list=list1, text='textey')
@@ -156,7 +165,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
         
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
